a165a7cb7f0d74673cf943fa230f3066249f21cb
Chris Rawles <crawles@gmail.com>
2017-06-02 12:28:47 -0400

    pyspark udf supports optional number of arguments

--- spark_udfs.py
+++ spark_udfs.py
@@ -20,7 +20,7 @@ def spark_udf(spark_type):
     return function_decorator
 
 
-def _apply_to_struct(fxn, return_type, struct_col):
+def _apply_to_struct(fxn, return_type, struct_col, **fxn_kwargs):
     """
     :param group_col: column name of a pyspark StructType
     :param fxn: function to apply to a Pandas DataFrame
@@ -32,16 +32,17 @@ def _apply_to_struct(fxn, return_type, struct_col):
         first_row = _struct_col[0]
         col_names = first_row.__fields__
         df = pd.DataFrame(_struct_col, columns=col_names)
-        return fxn(df)
+        return fxn(df,**fxn_kwargs)
 
     return _apply_fxn(struct_col)
 
 
-def apply_py_fxn(fxn, return_type, *cols):
+def apply_py_fxn(fxn, return_type, *cols, **fxn_kwargs):
+    print(fxn_kwargs)
     struct_args = [F.col(c) for c in cols]
     struct = F.struct(*struct_args)
     struct_list = F.collect_list(struct)
-    return _apply_to_struct(fxn, return_type, struct_list).alias(fxn.__name__)
+    return _apply_to_struct(fxn, return_type, struct_list, **fxn_kwargs).alias(fxn.__name__)
 
 
 
